e1b6363d2f4be15ca4d21ba1720199ce5c08a8e5
Justina1106 <jbycenkova@gmail.com>
2023-11-20 22:16:02 +0200

    Day-night mygtukas added

--- FitnetFood/src/App.jsx
+++ FitnetFood/src/App.jsx
@@ -3,18 +3,23 @@ import Home from "./pages/Home";
 import MealKitForm from "./pages/MealKitForm";
 import Cart from "./pages/Cart";
 import Header from "./components/Header";
+import { ThemeProvider } from "./context/ThemeContext";
+import AppWrapper from "./components/AppWrapper";
 
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider>
+      <AppWrapper>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/new" element={<MealKitForm />} />
       </Routes>
-    </>
+      </AppWrapper>
+    </ThemeProvider>
+  
   );
 };
 
--- FitnetFood/src/components/Header.jsx
+++ FitnetFood/src/components/Header.jsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../context/ThemeContext";
+import Button from "./Button";
+import { FaRegSun, FaRegMoon } from "react-icons/fa";
+
+
+
 import "./Header.scss";
 
 const Header = () => {
+  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+
   return (
     <header className="top-header">
       <nav>
-       
+      <div className="links">
         <img src="https://fitnetfood.lt/wp-content/uploads/2019/11/subalansuotas-maistas-i-namus-fitnetfood.lt-logo-120x128.png" width={75} alt="Logo" />
         <Link to="/">Pradžia</Link>
         <Link to="/">Maisto rinkiniai</Link>
@@ -13,7 +22,10 @@ const Header = () => {
         <Link to="/">Planų kainos</Link>
         <Link to="/cart">Krepšelis</Link>
         <Link to="/new">Naujas mitybos planas</Link>
-       
+        </div>
+        <Button onClick={toggleTheme}>
+          {isDarkMode ? <FaRegSun /> : <FaRegMoon />}
+        </Button>
       </nav>
     </header>
   );
