b04c6d87b4fe2eec9589ca2bc56a08b69a27f8f1
Justina1106 <jbycenkova@gmail.com>
2023-12-10 15:25:50 +0200

    Pagerintas Cart Css failas

--- Eshop/src/pages/cart/cart.jsx
+++ Eshop/src/pages/cart/cart.jsx
@@ -32,7 +32,7 @@ const Cart = () => {
 </div>
 ) : (
     <h1> Your Cart is Empty</h1>
-)};
+)}
     </div>
     ); 
 };
--- Eshop/src/products.js
+++ Eshop/src/products.js
@@ -3,28 +3,28 @@ const PRODUCTS = [
         id: 1,
         productName: "Čili Con Carne sriuba",
         price: 2.80,
-        productImage: "https://cdn-dserve.imgix.net/ab8a3c3e-adbf-4e12-9cc9-044bc8424d0e.jpg?h=472&w=440",
+        productImage: "https://cdn-dserve.imgix.net/ab8a3c3e-adbf-4e12-9cc9-044bc8424d0e.jpg",
     },
 
     {
         id: 2,
         productName: "Lėtai keptas kiaulienos sprandinės kepsnys",
         price: 5.90,
-        productImage: "https://cdn-dserve.imgix.net/860fcdee-e755-4d07-8c7d-4d25036877bb.jpg?h=472&w=440",
+        productImage: "https://cdn-dserve.imgix.net/860fcdee-e755-4d07-8c7d-4d25036877bb.jpg",
     },
 
     {
         id: 3,
         productName: "Traškūs cepelinai su mėsa",
         price: 5.50,
-        productImage: "https://cdn-dserve.imgix.net/04608014-0385-499c-a264-c11f2721b6d9.jpg?h=472&w=440",
+        productImage: "https://cdn-dserve.imgix.net/04608014-0385-499c-a264-c11f2721b6d9.jpg",
     },
 
     {
         id: 4,
         productName: "BBQ kiaulienos šonkauliai",
         price: 6.50,
-        productImage: "https://cdn-dserve.imgix.net/90fa3313-40f1-468b-9553-6501e578257a.jpg?h=472&w=440",
+        productImage: "https://cdn-dserve.imgix.net/90fa3313-40f1-468b-9553-6501e578257a.jpg",
     },
 
     {
